7a73b7cab6a33f2eb622bff47839bfd9c681e72a
YAHsaveme <hoangdvse183095@fpt.edu.vn>
2025-06-13 23:07:47 +0700

    LAB1

---
+++ Services/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using BusinessObjects;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public interface IProductService
+    {
+        void SaveProduct(Product p);
+        void DeleteProduct(Product p);
+        void UpdateProduct(Product p);
+        List<Product> GetProducts();
+        Product GetProductById(int id);
+    }
+}
+
